f3172eb51d044bbfd5e8589ab9bfbb35c85dbf99
meghadesai02 <91870526+meghadesai02@users.noreply.github.com>
2023-04-05 17:36:59 +0530

    Update and rename index.py to app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ def run():
     return "{\"message\" : \"Hey there python\"}"
 
 if __name__== "__main__":
-    helloworld.run(host="0.0.0.0.",port=int("3000"),debug=True)
+    helloworld.run(host="0.0.0.0.",port=int("8080"),debug=True)
