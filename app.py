19c561d2d09c2596bcd4b81ad8ca5285825f2a23
meghadesai02 <91870526+meghadesai02@users.noreply.github.com>
2023-04-05 17:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-helloworld = Flask(__name__)
-@helloworld.route("/")
-def run():
-    return "{\"message\" : \"Hey there python\"}"
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__== "__main__":
-    helloworld.run(host="0.0.0.0.",port=int("8080"),debug=True)
+    app.run(host="0.0.0.0.",port=int("8080"),debug=True)
